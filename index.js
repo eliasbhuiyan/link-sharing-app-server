5fbd0205ee002a1fe4213d6cd7acc44293f0b82c
Elias Bhuiyan <91391344+eliasbhuiyan@users.noreply.github.com>
2024-10-15 14:27:01 +0600

    Update index.js

--- index.js
+++ index.js
@@ -9,16 +9,15 @@ const app = express();
 app.use(express.json());
 app.use(cookieParser())
 
-// app.use(cors(
-//   {
-//     origin: [
-//       "http://localhost:5173",
-//     ],
-//     credentials: true
-//   }
-// ));
+app.use(cors(
+  {
+    origin: [
+      "https://link-sharing-app-eb.vercel.app",
+    ],
+    credentials: true
+  }
+));
 
-app.use(cors());
 dbconfig();
 app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 app.use(routes);
